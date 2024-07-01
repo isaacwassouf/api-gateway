fc404210b46670516c281a9ed2cb740c4456c77e
Isaac Wassouf <isaacwassouf@gmail.com>
2024-07-01 15:50:14 +0200

    WIP: add enpoint for listing a table data

--- src/routes/contents/index.ts
+++ src/routes/contents/index.ts
@@ -1,8 +1,32 @@
 import express from 'express';
 import { Request, Response } from 'express';
 import { Empty } from 'google-protobuf/google/protobuf/empty_pb';
-import { GetContentRequest } from '../../protobufs/content-service-protobufs/content-service_pb';
+import { ListContentRequest } from '../../protobufs/content-service-protobufs/content-service_pb';
 import { ContentManagementClient } from '../../services/contents';
 
 // Create a new router
 export const router = express.Router();
+
+router.get('/tables/:tableName', async (req: Request, res: Response) => {
+  const tableName = req.params.tableName;
+
+  ContentManagementClient.getInstance().listContent(
+    new ListContentRequest().setTableName(tableName).setPage(1).setPerPage(10),
+    (error, response) => {
+      if (error) {
+        return res.status(500).json({ error: error.message });
+      }
+
+      const entitiesList = response.getEntitiesList_asB64().map((entity) => {
+        return JSON.parse(Buffer.from(entity, 'base64').toString());
+      });
+
+      return res.json({
+        entities: entitiesList,
+        page: response.getPage(),
+        perPage: response.getPerPage(),
+        totalPages: response.getTotalPages(),
+      });
+    },
+  );
+});
