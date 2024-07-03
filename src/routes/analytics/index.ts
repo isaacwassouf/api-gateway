a37655b6cf20a9ea04b30bec1c3d10b08ae59958
Isaac Wassouf <isaacwassouf@gmail.com>
2024-07-03 22:36:44 +0200

    fix: add window for logging list

--- src/routes/analytics/index.ts
+++ src/routes/analytics/index.ts
@@ -3,31 +3,37 @@ import { Request, Response } from 'express';
 import { Empty } from 'google-protobuf/google/protobuf/empty_pb';
 import { AnalyticsClient } from '../../services/analytics';
 import { LogsList } from '../../types/analytics';
+import { ListLogsRequest } from '../../protobufs/analytics-service-protobufs/analytics-service_pb';
 
 // Create a new router
 export const router = express.Router();
 
 router.get('/', async (req: Request, res: Response) => {
-  console.log('GET /logs');
-  AnalyticsClient.getInstance().listLogs(new Empty(), (error, response) => {
-    if (error) {
-      return res.status(500).json({ error: error.message });
-    }
+  const windowQuery = req.query.window;
+  const window = windowQuery ? parseInt(windowQuery.toString()) : 0;
 
-    const logsList: LogsList = {
-      logs: [],
-    };
+  AnalyticsClient.getInstance().listLogs(
+    new ListLogsRequest().setWindow(window),
+    (error, response) => {
+      if (error) {
+        return res.status(500).json({ error: error.message });
+      }
 
-    response.getLogsList().forEach((log) => {
-      logsList.logs.push({
-        service: log.getServiceName(),
-        level: log.getLevel(),
-        message: log.getResponseMessage(),
-        createdAt: log.getCreatedAt(),
-        metaData: log.getMetadata(),
+      const logsList: LogsList = {
+        logs: [],
+      };
+
+      response.getLogsList().forEach((log) => {
+        logsList.logs.push({
+          service: log.getServiceName(),
+          level: log.getLevel(),
+          message: log.getResponseMessage(),
+          createdAt: log.getCreatedAt(),
+          metaData: log.getMetadata(),
+        });
       });
-    });
 
-    return res.status(200).json(logsList);
-  });
+      return res.status(200).json(logsList);
+    },
+  );
 });
